efea6b6e17c682ca3d637af167daeb5c629e5072
isatatajuba <isadoratatajuba1806@gmail.com>
2024-09-02 22:49:44 -0300

    desafio

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react';
+import { Text, View } from 'react-native';
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+
+function HomeScrenn(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    ‹View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      ‹Text>Customizar para Azul</Text>
     </View>
   );
+    
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App() {
+  return (
+    ‹NavigationContainer>
+      ‹Stack Navigator>
+        < Stack.Screen 
+        name="Home"
+        component={ HomeScreen}
+        options={{ title: 'Principal' }}
+      />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
